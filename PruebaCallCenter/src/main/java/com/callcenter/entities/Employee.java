2dc770adcdbfa66653046b4a526196b9337f7097
gustavo10xx <gustavo.a.clavijo@hotmail.com>
2019-04-28 20:21:15 -0500

    ajuste librerias

--- PruebaCallCenter/src/main/java/com/callcenter/entities/Employee.java
+++ PruebaCallCenter/src/main/java/com/callcenter/entities/Employee.java
@@ -1,20 +1,43 @@
 package com.callcenter.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class Employee {
 
 	private long id;
 	private String role;
 	private Boolean available;
 	private int orderAttention;
+	
+	public Employee(long id, String role, Boolean available, int orderAttention) {
+		super();
+		this.id = id;
+		this.role = role;
+		this.available = available;
+		this.orderAttention = orderAttention;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Boolean getAvailable() {
+		return available;
+	}
+	public void setAvailable(Boolean available) {
+		this.available = available;
+	}
+	public int getOrderAttention() {
+		return orderAttention;
+	}
+	public void setOrderAttention(int orderAttention) {
+		this.orderAttention = orderAttention;
+	}
+	
 }
